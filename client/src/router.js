1de2a819c6894e0a820cfaa630dde5962919e7a0
zhaowangdaren <gtja@gtjadeMac-mini.local>
2017-08-10 21:54:26 +0800

    MOD
    Lazy router

--- client/src/router.js
+++ client/src/router.js
@@ -3,6 +3,41 @@ import Router from 'vue-router'
 
 Vue.use(Router)
 
+const Login = resolve => require(['@/pages/login'], resolve)
+const Admin = resolve => require(['@/pages/admin'], resolve)
+const AdminBasicInfo = resolve => require(['@/pages/admin/basicInfo'], resolve)
+const AdminUsers = resolve => require(['@/pages/admin/users'], resolve)
+const AdminInit = resolve => require(['@/pages/admin/init'], resolve)
+const AdminWX = resolve => require(['@/pages/admin/wx'], resolve)
+const AdminComplaint = resolve => require(['@/pages/admin/complaints'], resolve)
+const AdminPM = resolve => require(['@/pages/admin/pm'], resolve)
+const AdminBuild = resolve => require(['@/pages/admin/build'], resolve)
+const Street = resolve => require(['@/pages/street'], resolve)
+const StreetIndex = resolve => require(['@/pages/street/index'], resolve)
+const StreetDetailEvent = resolve => require(['@/components/DetailEvent'], resolve)
+const StreetEventHandle = resolve => require(['@/components/table/EventHandles'], resolve)
+const StreetPM= resolve => require(['@/pages/street/pms'], resolve)
+const StreetPMKPI= resolve => require(['@/pages/street/pmsKPI'], resolve)
+const StreetCourts= resolve => require(['@/components/table/Courts'], resolve)
+const StreetRecs= resolve => require(['@/pages/street/recs'], resolve)
+const Gov = resolve => require(['@/pages/gov'], resolve)
+const GovIndex = resolve => require(['@/pages/gov/index'], resolve)
+const GovEventHandle = resolve => require(['@/pages/gov/eventHandle'], resolve)
+const GovPM = resolve => require(['@/pages/gov/pm'], resolve)
+const GovBuild = resolve => require(['@/pages/gov/build'], resolve)
+const GovPMKPI = resolve => require(['@/pages/gov/pmkpi'], resolve)
+const GovCourt = resolve => require(['@/pages/gov/court'], resolve)
+const GovAnnouncement = resolve => require(['@/pages/gov/announcement'], resolve)
+const WXStartComplaint = resolve => require(['@/pages/wx/startComplaint'], resolve)
+const WXComplaintSelectStreet = resolve => require(['@/pages/wx/complaint/selectStreet'], resolve)
+const WXComplaintSelectCommunity = resolve => require(['@/pages/wx/complaint/selectCommunity'], resolve)
+const WXComplaintSelectXQ = resolve => require(['@/pages/wx/complaint/selectXQ'], resolve)
+const WXComplaintAddEvent = resolve => require(['@/pages/wx/complaint/addEvent'], resolve)
+const WXComplaintUploadImage = resolve => require(['@/pages/wx/complaint/uploadImage'], resolve)
+const WXComplaintFinish = resolve => require(['@/pages/wx/complaint/finish'], resolve)
+const WXViewProgress = resolve => require(['@/pages/wx/viewProgress'], resolve)
+const WXDetailsProgress = resolve => require(['@/pages/wx/progress/detailsProgress'], resolve)
+
 export default new Router({
   // mode: 'history',
   routes: [
@@ -16,138 +51,137 @@ export default new Router({
     },
     {
       path: '/login',
-      component: require('@/pages/login')
+      component: Login
     },
     {
       path: '/admin',
-      component: require('@/pages/admin'),
+      component: Admin,
       children: [
         {
           path:'',
-          component: require('@/pages/admin/basicInfo')
+          component: AdminBasicInfo
         },
         {
           path: 'users',
-          component: require('@/pages/admin/users')
+          component: AdminUsers
         },{
           path: 'init',
-          component: require('@/pages/admin/init')
+          component: AdminInit
         },{
           path: 'wx',
-          component: require('@/pages/admin/wx')
+          component: AdminWX
         },{
           path: 'complaints',
-          component: require('@/pages/admin/complaints')
+          component: AdminComplaint
         },{
           path: 'pm',
-          component: require('@/pages/admin/pm')
+          component: AdminPM
         },{
           path: 'build',
-          component: require('@/pages/admin/build')
+          component: AdminBuild
         },
       ]
     },
     {
       path: '/street',
-      component: require('@/pages/street'),
+      component: Street,
       children: [
         {
           path: '',
-          component: require('@/pages/street/index')
+          component: StreetIndex
         },
         {
           path: 'eventDetail/:index',
           name: 'eventDetail',
-          component: require('@/components/DetailEvent')
+          component: StreetDetailEvent
         },
         {
           path: 'handle',
-          component: require('@/components/table/EventHandles')
+          component: StreetEventHandle
         },
         {
           path: 'pms',
-          component: require('@/pages/street/pms')
+          component: StreetPM
         },
         {
           path: 'pmkpis',
-          component: require('@/pages/street/pmsKPI')
+          component: StreetPMKPI
         },
         {
           path: 'court',
-          component: require('@/components/table/Courts')
+          component: StreetCourts
         },
         {
           path: 'operationRecs',
-          component: require('@/pages/street/recs')
+          component: StreetRecs
         }
       ]
     },
     {
       path: '/gov',
-      component: require('@/pages/gov'),
+      component: Gov,
       children: [
         {
           path:'',
-          component: require('@/pages/gov/index')
+          component: GovIndex
         },
         {
           path:'eventHandle',
-          component: require('@/pages/gov/eventHandle')
+          component: GovEventHandle
         },{
           path:'pms',
-          component: require('@/pages/gov/pm')
+          component: GovPM
         },{
           path:'build',
-          component: require('@/pages/gov/build')
+          component: GovBuild
         },{
           path:'pmkpis',
-          component: require('@/pages/gov/pmkpi')
+          component: GovPMKPI
         },{
           path:'court',
-          component: require('@/pages/gov/court')
+          component: GovCourt
         },{
           path:'announcement',
-          component: require('@/pages/gov/announcement')
+          component: GovAnnouncement
         },
       ]
     },
     {
       path: '/wx/startComplaint',
-      component: require('@/pages/wx/startComplaint')
+      component: WXStartComplaint
     },
     {
       path: '/wx/complaint/selectStreet',
-      component: require('@/pages/wx/complaint/selectStreet')
+      component: WXComplaintSelectStreet
     },
     {
       path: '/wx/complaint/selectCommunity',
-      component: require('@/pages/wx/complaint/selectCommunity')
+      component: WXComplaintSelectCommunity
     },
     {
       path: '/wx/complaint/selectXQ',
-      component: require('@/pages/wx/complaint/selectXQ')
+      component: WXComplaintSelectXQ
     },
     {
       path: '/wx/complaint/addEvent',
-      component: require('@/pages/wx/complaint/addEvent')
+      component: WXComplaintAddEvent
     },
     {
       path: '/wx/complaint/uploadImage',
-      component: require('@/pages/wx/complaint/uploadImage')
+      component: WXComplaintUploadImage
     },
     {
       path: '/wx/complaint/finish',
-      component: require('@/pages/wx/complaint/finish')
+      component: WXComplaintFinish
     },
     {
       path: '/wx/viewProgress',
-      component: require('@/pages/wx/viewProgress')
+      component: WXViewProgress
     },
     {
       path: '/wx/detailsProgress',
       name: 'detailsProgress',
-      component: require('@/pages/wx/progress/detailsProgress')
+      component: WXDetailsProgress
     }
-
   ]
 })
