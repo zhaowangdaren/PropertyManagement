e978b69297b7d46b318a8337f5e7b3e373e16c3b
zhaowangdaren <gtja@gtjadeMac-mini.local>
2017-09-08 01:01:56 +0800

    MOD
    2222

--- server/server/eventHandle.go
+++ server/server/eventHandle.go
@@ -183,7 +183,7 @@ func startEventHandle(router *gin.RouterGroup, dbc *mgo.Database) {
 		xqid := c.Query("xqid")
 		pm := table.FindPMByKV(dbc, "xqid", xqid)
 		pmUser := table.FindPMUserByKV(dbc, "pmid", string(pm.ID))
-		if pmUser.OpenId == "" {
+		if pmUser.OpenID == "" {
 			c.JSON(http.StatusOK, gin.H{"error": 1, "data": "该小区物业人员未绑定微信"})
 			return
 		}
